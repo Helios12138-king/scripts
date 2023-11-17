ea6c22b93f82053847fb535d2507bd9531b299c7
Helios12138-king <74335189+Helios12138-king@users.noreply.github.com>
2023-11-17 12:38:09 +0800

    Update tianqi.js

--- tianqi.js
+++ tianqi.js
@@ -18,7 +18,7 @@ $httpClient.get(apiUrl, (error, response, data) => {
 
   const cityInfo = weatherData.cityInfo;
   const currentWeather = weatherData.data.forecast[0];
-    const message = `城市：${cityInfo.city}\n更新时间：${cityInfo.updateTime} \n天气：${currentWeather.type}\n温度：${currentWeather.low}  ${currentWeather.high}\n湿度：${weatherData.data.shidu}\n空气质量：${weatherData.data.quality}\nPM2.5：${weatherData.data.pm25}\nPM10：${weatherData.data.pm10}\n风向：${currentWeather.fx}\n风力：${currentWeather.fl}\n日出时间：${currentWeather.sunrise}\n日落时间：${currentWeather.sunset}\n提示：${currentWeather.notice}`;
+    const message = `城市：${cityInfo.city}\n更新时间：${cityInfo.updateTime} \n天气：${currentWeather.type}\n温度：${currentWeather.low}  ${currentWeather.high}\n湿度：${weatherData.data.shidu}\n空气质量：${weatherData.data.quality}\nPM2.5：${weatherData.data.pm25}\n风力：${currentWeather.fl}\n日出时间：${currentWeather.sunrise}\n日落时间：${currentWeather.sunset}\n${currentWeather.notice}`;
 
   const body = {
     title: "今日天气",
